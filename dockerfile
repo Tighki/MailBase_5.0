93b105f27e3ae732dca65056608cd3b92205ca3c
Tighki <tighki@mail.ru>
2025-07-10 09:18:27 +0000

    fix_db

--- dockerfile
+++ dockerfile
@@ -3,5 +3,6 @@ FROM php:8.2-apache
 RUN a2enmod rewrite
 RUN \
     apt-get update && \
-    apt-get install -y libldap2-dev libicu-dev
-RUN docker-php-ext-install mysqli pdo_mysql pdo ldap intl
+    apt-get install -y libldap2-dev libicu-dev libpng-dev libjpeg-dev libfreetype6-dev libzip-dev libexif-dev
+RUN docker-php-ext-configure gd --with-freetype --with-jpeg
+RUN docker-php-ext-install mysqli pdo_mysql pdo ldap intl gd zip exif
--- src/config/config.inc.php
+++ src/config/config.inc.php
@@ -6,133 +6,164 @@
 // SQL DATABASE
 // ----------------------------------
 // Database connection string (DSN) for read+write operations
-// Format (compatible with PEAR MDB2): db_provider://user:password@host/database
-// Currently supported db_providers: mysql, pgsql, sqlite, mssql, sqlsrv, oracle
-// For examples see http://pear.php.net/manual/en/package.database.mdb2.intro-dsn.php
-// Note: for SQLite use absolute path (Linux): 'sqlite:////full/path/to/sqlite.db?mode=0646'
-//       or (Windows): 'sqlite:///C:/full/path/to/sqlite.db'
-// Note: Various drivers support various additional arguments for connection,
-//       for Mysql: key, cipher, cert, capath, ca, verify_server_cert,
-//       for Postgres: application_name, sslmode, sslcert, sslkey, sslrootcert, sslcrl, sslcompression, service.
-//       e.g. 'mysql://roundcube:@localhost/roundcubemail?verify_server_cert=false'
-// $config['db_dsnw'] = 'mysql://root:php_mysql_pass@database-container/roundcube';
 $config['db_dsnw'] = 'mysql://postfix:postfix_pass@database-container/postfix';
+
+// Table prefix for Roundcube tables
+$config['db_prefix'] = '';
+
+// ----------------------------------
+// LOGGING
+// ----------------------------------
+// Logging driver: 'syslog', 'stdout', 'file' or null
+$config['log_driver'] = 'file';
+
+// Directory for storing log files (must be writeable for webserver)
+$config['log_dir'] = '/var/www/html/logs/';
+
+// Syslog facility to use, if using the 'syslog' log driver
+$config['syslog_facility'] = LOG_MAIL;
+
+// Log identification string to use for syslog driver
+$config['syslog_id'] = 'roundcube';
+
 // ----------------------------------
 // IMAP
 // ----------------------------------
-// The IMAP host chosen to perform the log-in.
-// Leave blank to show a textbox at login, give a list of hosts
-// to display a pulldown menu or set one host as string.
-// Enter hostname with prefix ssl:// to use Implicit TLS, or use
-// prefix tls:// to use STARTTLS.
-// Supported replacement variables:
-// %n - hostname ($_SERVER['SERVER_NAME'])
-// %t - hostname without the first part
-// %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
-// %s - domain name after the '@' from e-mail address provided at login screen
-// For example %n = mail.domain.tld, %t = domain.tld
-// WARNING: After hostname change update of mail_host column in users table is
-//          required to match old user data records with the new host.
+// The IMAP host chosen to perform the log-in
+$config['default_host'] = 'dovecot';
 
-// $config['default_host'] = '185.120.59.214';
-
-// ----------------------------------
-// SMT
-// ----------------------------------
-// SMTP server host (for sending mails).
-// Enter hostname with prefix ssl:// to use Implicit TLS, or use
-// prefix tls:// to use STARTTLS.
-// Supported replacement variables:
-// %h - user's IMAP hostname
-// %n - hostname ($_SERVER['SERVER_NAME'])
-// %t - hostname without the first part
-// %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
-// %z - IMAP domain (IMAP hostname without the first part)
-// For example %n = mail.domain.tld, %t = domain.tld
-// To specify different SMTP servers for different IMAP hosts provide an array
-// of IMAP host (no prefix or port) and SMTP server e.g. ['imap.example.com' => 'smtp.example.net']
-$config['smtp_server'] = '185.120.59.214';
-
-// provide an URL where a user can get support for this Roundcube installation
-$config['enable_installer'] = false;
-// PLEASE DO NOT LINK TO THE ROUNDCUBE.NET WEBSITE HERE!
-$config['support_url'] = '';
-
-// This key is used for encrypting purposes, like storing of imap password
-// in the session. For historical reasons it's called DES_key, but it's used
-// with any configured cipher_method (see below).
-// For the default cipher_method a required key length is 24 characters.
-$config['des_key'] = 'MKXB3PukpBKj6mSYZEN4An5Y';
+// TCP port used for IMAP connections
+$config['default_port'] = 143;
 
 // Automatically add this domain to user names for login
-// Only for IMAP servers that require full e-mail addresses for login
-// Specify an array with 'host' => 'domain' values to support multiple hosts
-// Supported replacement variables:
-// %h - user's IMAP hostname
-// %n - hostname ($_SERVER['SERVER_NAME'])
-// %t - hostname without the first part
-// %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
-// %z - IMAP domain (IMAP hostname without the first part)
-// For example %n = mail.domain.tld, %t = domain.tld
-$config['username_domain'] = 'эксперты-1с.рф';
+// Using Punycode for Cyrillic domain compatibility
+$config['username_domain'] = 'xn----7sbbh1akdldfh0ch.xn--p1ai';
+
+// Automatically create a new Roundcube user when log-in the first time
+$config['auto_create_user'] = true;
+
+// Store sent messages in this folder
+$config['sent_mbox'] = 'Sent';
+
+// Move messages to this folder when deleting them
+$config['trash_mbox'] = 'Trash';
+
+// Store draft messages in this folder
+$config['drafts_mbox'] = 'Drafts';
+
+// Store spam messages in this folder
+$config['junk_mbox'] = 'Junk';
+
+// ----------------------------------
+// SMTP
+// ----------------------------------
+// SMTP server host (for sending mails)
+$config['smtp_server'] = 'postfix';
+
+// SMTP port
+$config['smtp_port'] = 587;
+
+// Use current IMAP username and password for SMTP authentication
+$config['smtp_user'] = '%u';
+$config['smtp_pass'] = '%p';
+
+// Log sent messages in log_dir/sendmail or to syslog
+$config['smtp_log'] = true;
+
+// ----------------------------------
+// SYSTEM
+// ----------------------------------
+// Provide URL where a user can get support for this Roundcube installation
+$config['support_url'] = 'mailto:admin@xn----7sbbh1akdldfh0ch.xn--p1ai';
 
 // Name your service. This is displayed on the login screen and in the window title
 $config['product_name'] = 'FltrWebmail';
+
+// Use this folder to store temp files (must be writeable for webserver)
+$config['temp_dir'] = '/var/www/html/temp/';
+
+// This key is used for encrypting purposes, like storing of imap password
+$config['des_key'] = 'MKXB3PukpBKj6mSYZEN4An5Y';
+
+// Check client IP in session authorization
+$config['ip_check'] = true;
+
+// Enable spellchecker
+$config['enable_spellcheck'] = true;
+
+// Spellcheck engine
+$config['spellcheck_engine'] = 'googie';
+
+// Set identities access level:
+// 2 - one identity with possibility to edit all params
+$config['identities_level'] = 2;
+
+// ----------------------------------
+// USER INTERFACE
+// ----------------------------------
+// The default locale setting (leave empty for auto-detection)
+$config['language'] = 'ru_RU';
+
+// Skin name: folder from skins/
+$config['skin'] = 'elastic';
+
+// Show up to X items in the mail messages list view
+$config['mail_pagesize'] = 50;
+
+// Show up to X items in the contacts list view
+$config['addressbook_pagesize'] = 50;
+
+// Prefer displaying HTML messages
+$config['prefer_html'] = true;
+
+// Compose html formatted messages by default
+// 1 - always
+$config['htmleditor'] = 1;
+
+// Save compose message every X minutes
+$config['draft_autosave'] = 300;
+
+// Behavior if a received message requests a message delivery notification (read receipt)
+$config['mdn_requests'] = 0;
+
+// How to encode attachment long/non-ascii names
+$config['mime_param_folding'] = 1;
+
+// ----------------------------------
+// SECURITY
+// ----------------------------------
+// Enforce connections over https
+// $config['force_https'] = true;
+
+// Use secure flag for session cookies
+$config['session_auth_name'] = 'roundcube_sessauth';
+
+// Session domain
+// $config['session_domain'] = '.эксперты-1с.рф';
 
 // ----------------------------------
 // PLUGINS
 // ----------------------------------
 // List of active plugins (in plugins/ directory)
 $config['plugins'] = [
-	 'fetchmail',
-	 'persistent_login',
-	 'filters',
-	 'userinfo',
-	 'emoticons',
-	 'newmail_notifier',
-	 'reconnect'];
+    'archive',
+    'attachment_reminder', 
+    'emoticons',
+    'enigma',
+    'fetchmail',
+    'filters',
+    'help',
+    'hide_blockquote',
+    'newmail_notifier',
+    'password',
+    'persistent_login',
+    'reconnect',
+    'userinfo',
+    'zipdownload'
+];
 
-// the default locale setting (leave empty for auto-detection)
-// RFC1766 formatted language name like en_US, de_DE, de_CH, fr_FR, pt_BR
-$config['language'] = 'ru_RU';
-
-// $config['autocomplete_addressbooks'] = array('sql','ldap');
-
-$config['autocomplete_addressbooks'] = array('sql');
-
-// $config['ldap_public'] = array(
-//     'ldap' =>array (
-//         'name' => 'Общая адресная книга',
-//         'hosts' => array('openldap'),
-//         'sizelimit' => 600,
-//         'port' => 389,
-//         'use_tls' => false,
-//         'user_specific' => false,
-//         'base_dn' => 'ou=addressbook,dc=ramhlocal,dc=com',
-//         'bind_dn' => 'cn=admin,dc=ramhlocal,dc=com',//cn=admin,dc=ramhlocal,dc=com
-//         'bind_pass' => 'admin_pass',
-//         'writable' => false,
-//         'ldap_version' => 3,
-//         'search_fields' => array(
-//            'mail',
-//            'cn',
-//         ),
-//         'name_field' => 'cn',
-//         'email_field' => 'mail',
-//         'surname_field' => 'sn',
-//         'firstname_field' => 'givenName',
-//         //'organization_field'     => 'company',
-//         //'jobtitle_field'    => 'title',
-//         //'department_field'   => 'department',
-//         'sort' => 'sn',
-//         'scope' => 'sub',
-//         'filter' => '(&(objectClass=*))',
-//         //'filter' => '(&(mail=*)(|(&(objectcategory=person)(!(objectClass=computer)))(objectClass=group)))',
-//         'global_search' => true,
-//         'fuzzy_search' => true
-//     ),
-// );
-
-// $config['ldap_cache'] = 'db';
-// $config['ldap_cache_ttl'] = '10m';
-$config['max_message_size'] = '100M';
+// ----------------------------------
+// FETCHMAIL PLUGIN CONFIG
+// ----------------------------------
+// Enable fetchmail plugin
+$config['fetchmail_enabled'] = true;
